e45f508c63c291eb666f7dbdbe8bcd418d314598
heyanlin <heyanlin@tencent.com>
2024-02-29 17:28:35 +0800

    1.数据库架构

---
+++ lib/db/manga/entity/chapter.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chapter.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ChapterImpl _$$ChapterImplFromJson(Map<String, dynamic> json) =>
+    _$ChapterImpl(
+      label: json['label'] as String,
+      id: json['id'] as String,
+    );
+
+Map<String, dynamic> _$$ChapterImplToJson(_$ChapterImpl instance) =>
+    <String, dynamic>{
+      'label': instance.label,
+      'id': instance.id,
+    };
