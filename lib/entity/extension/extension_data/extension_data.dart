efaa40703c3b53316650b7d75170ea4e0cb6e4bf
heyanLE <1371735400@qq.com>
2024-04-02 20:11:10 +0800

    1.拓展加载

--- lib/entity/extension/extension_data/extension_data.dart
+++ lib/entity/extension/extension_data/extension_data.dart
@@ -9,14 +9,14 @@ part 'extension_data.freezed.dart';
 /// ExtensionInfo 加载结果
 /// 拓展管理界面依赖的实体
 
-enum ExtensionState { loading, none, loaded, error }
+enum ExtensionState { loaded, error }
 
 @freezed
 class ExtensionData with _$ExtensionData {
   factory ExtensionData({
     required ExtensionInfo info,
     @Default(null) List<SourceInfo>? sources,
-    @Default(ExtensionState.none) ExtensionState state,
+    @Default(ExtensionState.loaded) ExtensionState state,
     @JsonKey(name: "error_msg") @Default("") String errorMsg,
     // 该插件的文件夹
     @JsonKey(name: "folder_path") required String folderPath,
