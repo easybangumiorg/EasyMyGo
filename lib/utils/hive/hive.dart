36b555390cde1d2007c4e7148c8fb0d5ec3fe503
AyaraKaguya <ayalakaguya@icloud.com>
2024-08-28 16:49:05 +0800

    update: 将hive升级为hive-ce

--- lib/utils/hive/hive.dart
+++ lib/utils/hive/hive.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
 import 'package:easy_mygo/c.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_ce/hive.dart';
 import 'package:path/path.dart';
 
 class HiveBox {
-
   static final init = Future(() async {
     final applicationDir = await EasyConstant.applicationPath;
     Hive.init(join(applicationDir.path, "hive"));
@@ -27,20 +26,16 @@ class HiveBox {
     await init;
     return Hive.openLazyBox<Map>("config");
   }
-
-
 }
 
 extension BoxExt<E> on Box<E> {
-  E? getSingle({E? defaultValue}) => get(HiveBox.key, defaultValue: defaultValue);
+  E? getSingle({E? defaultValue}) =>
+      get(HiveBox.key, defaultValue: defaultValue);
   void putSingle(E value) => put(HiveBox.key, value);
 }
 
 extension LazyBoxExt<E> on LazyBox<E> {
-  Future<E?> getSingle({E? defaultValue}) => get(HiveBox.key, defaultValue: defaultValue);
+  Future<E?> getSingle({E? defaultValue}) =>
+      get(HiveBox.key, defaultValue: defaultValue);
   Future<void> putSingle(E value) => put(HiveBox.key, value);
 }
-
-
-
-
